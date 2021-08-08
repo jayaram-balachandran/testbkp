4f4e582aa7a558965d94d8b0e99178e12fec62dc
jayaram-balachandran <62475283+jayaram-balachandran@users.noreply.github.com>
2021-08-08 16:45:10 +0530

    Create admin.ts

---
+++ admin.ts
@@ -0,0 +1,9 @@
+export class Admin {
+  public emailId?: string;
+  public firstName?: string;
+  public lastName?: string;
+  public language?: string;
+  public province?: string;
+  public group?: string;
+  public status?: string;
+}
